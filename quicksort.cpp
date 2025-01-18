2d7784f2d42f731d41b96b986533f94d1be30473
sayuj63 <sayujpillai63@gmail.com>
2025-01-18 17:50:32 +0530

    new

--- quicksort.cpp
+++ quicksort.cpp
@@ -14,7 +14,8 @@ int partition(int arr[], int low, int high) {
     return (i + 1);
 }
 
-void quickSort(int arr[], int low, int high) {
+void quickSort(int arr[], int low, int high) 
+{
     if (low < high) {
         int pi = partition(arr, low, high);
         quickSort(arr, low, pi - 1);
