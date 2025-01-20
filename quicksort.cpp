4abd8893bf89fe71aeda3a2ef21f15a4cb068d02
sayuj63 <sayujpillai63@gmail.com>
2025-01-20 12:56:47 +0530

    new

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int partition(int arr[], int low, int high) {
+int partition(int arr[], int low, int high)
+{
     int pivot = arr[high];
     int i = (low - 1);
     for (int j = low; j < high; j++) {
